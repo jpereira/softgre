d5d87d144cad56d508adcae0940544d375be3279
Jorge Pereira <jpereiran@gmail.com>
2014-09-29 18:38:38 -0300

    Improve the help showed in -h/--help

--- src/softgred_config.c
+++ src/softgred_config.c
@@ -120,15 +120,46 @@ softgred_print_usage(char *argv[])
 {
     const char *arg0 = basename (argv[0]);
 
-    printf("Usage: %s -i <ethX> [-a vlan_id1@bridge0 -a vlan_id2@bridge1 ...] [-p tun_prefi] [-f] [-d] [OPTIONS]\n", arg0);
-    printf("\n");
-    printf("   -i, --iface             Network interface to listen\n");
-    printf("   -a, --attach            Name of vlan/bridge to be attached, e.g: 10@br-vlan2410\n");
-    printf("   -p, --tunnel-prefix     Prefix name to be used in gre-network-interfaces, default e.g: %sN (N num. of instance)\n", SOFTGRED_TUN_PREFIX);
-    printf("   -f, --foreground        Run in the foreground\n");
-    printf("   -h, --help              Display this help text\n");
-    printf("   -d, --debug             Enable debug mode\n");
-    printf("   -v, --version           Display the %s version\n", arg0);
-    printf("\n");
+    printf("Usage: %s -i <ethX> [-a vlan_id1@bridge0 -a vlan_id2@bridge1 ...] [-p tun_prefi] [-f] [-d] [OPTIONS]\n" \
+           "\n" \
+           " OPTIONS\n" \
+           "\n" \
+           "   -i, --iface          Network interface to listen GRE packets, e.g: -i eth0\n" \
+           "   -a, --attach         Name of vlan/bridge to be attached, e.g: 10@br-vlan2410\n" \
+           "   -p, --tunnel-prefix  Prefix name to be used in gre-network-interfaces, default e.g: %sN (N num. of instance)\n" \
+           "   -f, --foreground     Run in the foreground\n"  \
+           "   -h, --help           Display this help text\n" \
+           "   -d, --debug          Enable debug mode\n"      \
+           "   -v, --version        Display the %s version\n" \
+           "\n" \
+           " EXAMPLE\n" \
+           "\n" \
+           "Example: We have a interface 'eth0' there is a 'endpoint' of GRE tunnel, and we have a\n" \
+           "trunk interface 'eth1'. that can reach many vlans, like 2420,2421 and 2422. Now, we would\n" \
+           "like to attach with some vlans over GRE tunnel. \n" \
+           "\n" \
+           " -> Create a bridge\n" \
+           " # ip link add br-vlan2420 type bridge\n" \
+           " # ip link add br-vlan2421 type bridge\n" \
+           " # ip link add br-vlan2422 type bridge\n" \
+           "\n" \
+           " -> Create the local VLANs\n" \
+           " # ip link add link eth1 name eth1.2420 up type vlan id 2420\n" \
+           " # ip link add link eth1 name eth1.2421 up type vlan id 2421\n" \
+           " # ip link add link eth1 name eth1.2422 up type vlan id 2422\n" \
+           "\n" \
+           " -> Attach the local VLANs with the bridges\n" \
+           " # ip link set eth1.2420 master br-vlan2420\n" \
+           " # ip link set eth1.2421 master br-vlan2421\n" \
+           " # ip link set eth1.2422 master br-vlan2422\n" \
+           "\n" \
+           " -> Now, we can begin the provision!\n" \
+           "\n" \
+           " # %s -i eth0 -a 10@br-vlan2420 -a 11@br-vlan2421 -a 12@br-vlan2422\n" \
+           "\n" \
+           " -> Conclusion\n" \
+           "\n" \
+           " Everything that arrive in GRE tunnel, will be forwarded to internals VLANs/bridge.\n" \
+           "\n", arg0, SOFTGRED_TUN_PREFIX, arg0, arg0);
 }
 
